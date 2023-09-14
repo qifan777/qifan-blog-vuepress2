9bc08603686bd292ad05bb9f1900ac8444e4f64d
起凡 <1507906763@qq.com>
2023-09-14 13:34:50 +0800

    删除旧博客

--- blog/.vuepress/navbar.ts
+++ blog/.vuepress/navbar.ts
@@ -1,4 +1,4 @@
-import {navbar} from "vuepress-theme-hope";
+import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
@@ -7,31 +7,32 @@ export default navbar([
     icon: "pen-to-square",
     prefix: "/project/",
     children: [
-      {text: '飞鸽邮筒', link: 'post-letter/'},
-      {text: '多模块微服务实战', link: 'microservice-practice/book'},
-      {text: 'ChatGPT助手', link: 'chatgpt-assistant/'}]
+      { text: '飞鸽邮筒', link: 'post-letter/' },
+      { text: '多模块微服务实战', link: 'microservice-practice/book' },
+      { text: 'ChatGPT助手', link: 'chatgpt-assistant/' },
+      { text: "起凡小商店", link: "qifan-shop" }],
   },
   {
     text: '工具分享',
-    icon:'screwdriver-wrench',
+    icon: 'screwdriver-wrench',
     prefix: "/tools/",
     children: [
-      {text: '私有ChatGPT搭建', link: 'chatgpt/'},
-      {text: '远程开发环境搭建', link: 'remote-develop/'},
-      {text: 'vue-press2搭建博客', link: 'vue-press2/'},
-      {text: 'CI/CD与Jenkins', link: 'jenkins/'},
+      { text: '私有ChatGPT搭建', link: 'chatgpt/' },
+      { text: '远程开发环境搭建', link: 'remote-develop/' },
+      { text: 'vue-press2搭建博客', link: 'vue-press2/' },
+      { text: 'CI/CD与Jenkins', link: 'jenkins/' },
     ]
   },
   {
     text: '知识分享',
     prefix: "/knowledge/",
-    icon:"book",
+    icon: "book",
     children: [
       {
         text: 'Spring',
         prefix: "spring/",
         children: [
-          {text: 'AOP', link: 'aop'}, {text: '如何发送http请求各种参数', link: 'http'}
+          { text: 'AOP', link: 'aop' }, { text: '如何发送http请求各种参数', link: 'http' }
         ]
       },
       {
