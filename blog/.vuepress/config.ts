0bcd0f9e3785099a80e10d1ef90c21952515e7d4
linjiacheng <1507906763@qq.com>
2024-01-12 15:00:05 +0800

    字典管理

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -2,7 +2,7 @@ import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-    base: '/blog/',
+    base: '/qifan-blog-vuepress2/',
     dest: 'blog/.vuepress/blog',
     lang: "zh-CN",
     title: "起凡Code闲聊",
